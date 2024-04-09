f6c63011acd48adbe86bb0b65c5d434c497437ec
Bardin08 <vbardin810@gmail.com>
2024-04-09 23:55:52 +0300

    Extend Constants.cs

--- DbSeeder/Constants.cs
+++ DbSeeder/Constants.cs
@@ -2,14 +2,25 @@
 
 namespace DbSeeder;
 
-public class Constants
+public static class Constants
 {
     // TODO: After .NET 9 release can be replaced with SearchValues<string>
-    public static HashSet<string> Keywords =
+    public static readonly HashSet<string> Keywords =
     [
-        "SELECT", "INSERT", "UPDATE", "DELETE", "CREATE", "ALTER", "DROP", "FROM", "WHERE", "AND", "OR", "NOT", "IN",
-        "JOIN", "ON", "GROUP BY", "ORDER BY", "ASC", "DESC", "AS", "NULL", "TRUE", "FALSE"
+        "SELECT", "INSERT", "UPDATE", "DELETE", "CREATE", "ALTER", "DROP", "FROM", "WHERE",
+        "JOIN", "ON", "GROUP BY", "ORDER BY", "ASC", "DESC", "AS", "NULL", "TRUE", "FALSE", "TABLE"
     ];
-    
-    public static SearchValues<char> Punctuation = SearchValues.Create("(),.;");
+
+    // TODO: After .NET 9 release can be replaced with SearchValues<string>
+    public static readonly HashSet<string> Operators =
+    [
+        "AND", "OR", "NOT", "IN"
+    ];
+
+    public static readonly HashSet<string> Constraints =
+    [
+        "PRIMARY KEY", "FOREIGN KEY", "NOT NULL", "UNIQUE"
+    ];
+
+    public static readonly SearchValues<char> Punctuation = SearchValues.Create("(),.;");
 }
